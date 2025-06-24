94a8a6e0f8278fc85b4692e61a99039115df0b27
sanket h <sanketghinchanal@gmail.com>
2025-06-25 00:35:24 +0530

    Made Dashboard render logic dynamic

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,17 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  masterData: any;
+  cardsData : any[] = [];
+
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get<any[]>('assets/global/dashboardMaster.json').subscribe(data => {
+      const dbCardsObj = data.find(item => item.id === 'DbCards');
+      this.cardsData = dbCardsObj.value;
+      console.log(this.cardsData);
+    })
   }
 
-  highlightCard(cardType : string): void {
-    switch (cardType){
-      case 'military':
-        
+  configurationSetting(cardTitle : String): void {
+    switch (cardTitle){
+      case "Forts":
+        document.documentElement.style.setProperty('--main-bgcolor', '#116530');
       break;
+      case 'default':
+        document.documentElement.style.setProperty('--main-bgcolor', '#ffffff');
+        break;
     }
   }
-
 }
