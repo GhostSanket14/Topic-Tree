0f1ca4498e903a365a5a265a75df4b712381c19a
sanket h <sanketghinchanal@gmail.com>
2025-07-20 23:42:43 +0530

    Forts and Fort engine restructure and Map feature updated

--- src/app/modules/categories/categories.module.ts
+++ src/app/modules/categories/categories.module.ts
@@ -4,9 +4,9 @@ import { CommonModule } from '@angular/common';
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { TreeCategoryComponent } from './tree-category/tree-category.component';
 import { MaterialsModule } from 'src/app/shared/materials/materials.module';
-import { TreeFortsBuilderComponent } from './tree-forts-builder/tree-forts-builder.component';
 import { SharedComponentModule } from "../../shared/shared-component/shared-component.module";
 import { TreeWeaponsBuilderComponent } from './tree-weapons-builder/tree-weapons-builder.component';
+import { TreeFortsBuilderComponent } from './tree-forts-builder/tree-forts-builder.component';
 
 
 @NgModule({
